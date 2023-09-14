ebe376aa3e1b642c9360a5131161104e3a1bd2ea
Jess Rodino <108949769+jessr0@users.noreply.github.com>
2023-09-14 15:21:48 -0600

    Update to fix index error
    Initial file indexed result_column to 4, which does not contain fires. Changed index to 3.

--- print_fires.py
+++ print_fires.py
@@ -2,7 +2,7 @@ import my_utils
 
 query_value='United States of America'
 query_column = 1
-result_column = 4
+result_column = 3
 file_name = 'Agrofood_co2_emission.csv'
 fires = my_utils.get_column(file_name, query_column, query_value, result_column)
 print(fires)
