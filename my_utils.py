80c127bfc4c19fa52617608edd6da04dd1c9e6bf
Jess Rodino <108949769+jessr0@users.noreply.github.com>
2023-09-14 14:39:13 -0600

    Update my_utils.py
    get_column now returns  desired result_column when filtering by desired value

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,13 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    array_con = []
+    with open(file_name, 'r') as f:
+        for line in f:
+            columns = line.strip().split(",")
+            array_con.append(columns)
+    new_array = []
+    for i in array_con:
+        for j in i:
+            if j == query_value:
+                new_array.append(i[result_column])
+                print(new_array)
+
