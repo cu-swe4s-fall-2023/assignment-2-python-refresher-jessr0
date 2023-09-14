f21d92e1a1c51ceee6b44951c857371cd9be2552
Jess Rodino <108949769+jessr0@users.noreply.github.com>
2023-09-14 15:35:33 -0600

    Result_column default 1, run file.
    Updated get_column function to use a default of 1 for result_column variable. Created run file for program. Defined function utility in readme.

--- print_fires.py
+++ print_fires.py
@@ -2,7 +2,6 @@ import my_utils
 
 query_value='United States of America'
 query_column = 1
-result_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = my_utils.get_column(file_name, query_column, query_value, result_column)
+fires = my_utils.get_column(file_name, query_column, query_value)
 print(fires)
